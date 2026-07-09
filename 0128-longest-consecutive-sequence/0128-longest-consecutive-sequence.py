8ea42825f78bbf3d522f925dc985c5c53f7e84ff
Akanksha167 <191556418+Akanksha167@users.noreply.github.com>
2026-07-09 23:30:14 +0530

    Time: 52 ms (45.17%), Space: 36.6 MB (37.6%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,7 +4,6 @@ class Solution:
         longest = 0
 
         for num in num_set:
-            # Start only if it's the beginning of a sequence
             if num - 1 not in num_set:
                 current = num
                 length = 1
